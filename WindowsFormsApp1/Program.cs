5aacd82a09ef8c9f6cf7f31c0610f55b5d6ea64d
Administrator.PL <Administrator.PL@PL001IT759W7P64>
2018-07-18 10:38:48 +0200

    Interfejs

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -17,10 +17,9 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-
-
             Application.Run(new Form1());
+
         }
+       
     }
 }
